d76189ef5c26f63ae2cfe144e4dad9323717b640
Stefan Roschewitz <5253061-schlumpfen@users.noreply.gitlab.com>
2022-12-06 21:21:56 +0100

    07 erstellt und Klasse angepasst

---
+++ 2022/07/tag07.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.util.*;
+
+public class tag07
+{
+    ArrayList<String> liste = new ArrayList<String>();
+    ArrayList<Stack>  stapel = new ArrayList<Stack>();
+    
+    Hashtable<Character, Character> ht1 = new Hashtable<>();
+    
+    public tag07()
+    {
+        try
+        {
+            einlesen();
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+        starone();
+        // startwo();
+    }
+    void starone(){
+        int gesamtwert = 0;
+        int wert = 0;
+        String[] paare, vorne, hinten;
+        String temp="",teil="";
+        for (String element : liste){
+            paare =   element.split(" ");
+            temp = paare[0];
+        }
+        
+       // temp="bvwbjplbgvbhsrlpgdmjqwftvncz";
+        
+        int j=0;
+    for(int i = 0; i < temp.length()-5; i++) {    
+            teil= temp.substring(i, i+4);
+            ht1.clear();
+            j=0;
+            for(int k=0;k<teil.length();k++){
+                System.out.println(k+" "+ teil.charAt(k));
+                if (!ht1.containsValue(teil.charAt(k))){
+                    ht1.put(teil.charAt(k), teil.charAt(k));
+                    j++;
+                }
+                else {
+                    System.out.println(k+" "+ teil.charAt(k));
+                }
+                
+            }
+            if(j==4) {
+                wert = i+4;
+                System.out.println(j+teil);
+                break;
+            }        
+        }
+        System.out.print("Stern Eins: "+wert);        
+
+
+    }
+    
+    
+    void startwo(){
+        int gesamtwert = 0;
+        int wert = 0;
+        String[] paare, vorne, hinten, temp;
+        for (String element : liste){
+            paare =   element.split(" ");
+
+        }
+        System.out.print("Stern Zwei: ");        
+
+
+    }
+    
+    
+    void einlesen() throws IOException {
+       File file = new File("input.txt"); 
+       String st;
+       BufferedReader br = new BufferedReader(new FileReader(file));
+       while ((st = br.readLine()) != null){
+           liste.add(st);
+           System.out.println(st);      
+        }
+    }
+}
