7dbd76ee7f7296f44ffda4abb0e08fe4295cd320
Stefan Roschewitz <5253061-schlumpfen@users.noreply.gitlab.com>
2022-12-10 18:12:20 +0100

    Tag 08 und 10

--- 2022/08/tag08.java
+++ 2022/08/tag08.java
@@ -5,7 +5,7 @@ public class tag08
 {
     ArrayList<String> liste = new ArrayList<String>();
     ArrayList<Stack>  stapel = new ArrayList<Stack>();
-    
+    int[][] forest = new int[5][5];
     Hashtable<Character, Character> ht1 = new Hashtable<>();
     
     public tag08()
@@ -18,46 +18,109 @@ public class tag08
         {
             ioe.printStackTrace();
         }
+        
+        forest = new int[liste.size()][liste.size()];
+        
+        String[] paare, vorne, hinten;
+        String temp="",teil="";
+        int j=0;
+        for (String element : liste){
+            for(int i=0;i<element.length();i++)
+            {
+                forest[j][i] = Integer.parseInt(element.substring(i,i+1));            
+            }
+            j++;
+        }
+
+        
+        
         starone();
         // startwo();
     }
     void starone(){
-        int gesamtwert = 0;
-        int wert = 0;
-        String[] paare, vorne, hinten;
-        String temp="",teil="";
-        for (String element : liste){
-            paare =   element.split(" ");
-            temp = paare[0];
-        }
-        
-       // temp="bvwbjplbgvbhsrlpgdmjqwftvncz";
-        
-        int j=0;
-    for(int i = 0; i < temp.length()-5; i++) {    
-            teil= temp.substring(i, i+4);
-            ht1.clear();
-            j=0;
-            for(int k=0;k<teil.length();k++){
-                System.out.println(k+" "+ teil.charAt(k));
-                if (!ht1.containsValue(teil.charAt(k))){
-                    ht1.put(teil.charAt(k), teil.charAt(k));
-                    j++;
+        int wert=forest[0].length*4-4;
+        boolean found=false;
+        for(int i=1;i < forest[0].length-1;i++)
+        {
+            for(int j=1;j < forest[i].length-1;j++)
+            {
+                // Alle Zeilen uns Spalten
+                // Von hier nach oben/unten/rechts/links schauen
+                // Wenn man bis zum Rand kommt dann zählen
+                
+                // nach oben 
+                
+                found=false;
+                for(int o=j-1;o>=0;o--){
+                    if(forest[i][j] <= forest[o][j])
+                    {
+                        found = false;
+                        System.out.println("o  "+i+" "+j+ " " +forest[i][j]);
+                        break;
+                    }
+                    found = true;
                 }
-                else {
-                    System.out.println(k+" "+ teil.charAt(k));
+                if(found){
+                    wert++;    
+                }
+                else{
+                    // nach unten
+                    found=false;
+                    for(int o=j;o<forest[i].length;o++){
+                        if(forest[i][j] <= forest[o][j])
+                        {
+                            found = false;
+                            System.out.println("u  "+i+" "+j+ " " +forest[i][j]);
+                            break;
+                        }
+                        found = true;
+                    }
+                    if(found){
+                        wert++;    
+                    }
+                    else{
+                        // links
+                        found=false;
+                        for(int o=i;o<forest[i].length;o++){
+                            if(forest[i][j] <= forest[i][o])
+                            {
+                                found = false;
+                                System.out.println("l  "+i+" "+j+ " " +forest[i][j]);
+                                break;
+                            }
+                            found = true;
+                        }
+                        if(found){
+                            wert++;    
+                        }
+                        else{
+                            // rechts
+                            found=false;
+                            for(int o=i-1;o>=0;o--){
+                                if(forest[i][j] <= forest[i][o])
+                                {
+                                    found = false;
+                                    System.out.println("r  "+i+" "+j+ " " +forest[i][j]);
+                                    break;
+                                }
+                                found = true;
+                            }
+                            if(found){
+                                wert++;    
+                            }
+                        }
+                    }
                 }
                 
-            }
-            if(j==4) {
-                wert = i+4;
-                System.out.println(j+teil);
-                break;
-            }        
-        }
+                // System.out.print(forest[i][j]);
+                
+                
+            }   
+            System.out.println();
+        }        
+        
+        
         System.out.print("Stern Eins: "+wert);        
-
-
     }
     
     
@@ -70,8 +133,6 @@ public class tag08
 
         }
         System.out.print("Stern Zwei: ");        
-
-
     }
     
     
