81514086241fbdd7da0ff2eca50c567273d9f43e
Stefan Roschewitz <5253061-schlumpfen@users.noreply.gitlab.com>
2023-12-04 00:21:36 +0100

    Erster Stern

--- 2023/03a.dart
+++ 2023/03a.dart
@@ -6,10 +6,10 @@ void main() async {
     // Pfad zur Datei
     // Die ersten beiden Zeichen der DART-Datei sind die Inputdatei.
     var filePath = Platform.script.pathSegments.last[0]+Platform.script.pathSegments.last[1]+'.txt';
-    /*
+    
     var gesamt1 =0;
-    var gesamt2 =0;
-    */
+    // var gesamt2 =0;
+    
     
     /*
     // Die Datei zeilenweise einlesen
@@ -26,35 +26,82 @@ void main() async {
       List<String> row = line.split('');
       schematic.add(row);
     }
+    int i=0;
+    int j=0;
     // Zeile für Zeile
-    for (int i = 0; i < schematic.length; i++) {
+    for (i = 0; i < schematic.length; i++) {
       // Spalte für Spalte
-      var strZahl = '';
-      for (int j = 0; j < schematic[i].length; j++) {
+      String strZahl = '';
+      for (j = 0; j < schematic[i].length; j++) {
           // wenn Ziffer dann merken!
           if(isDigit(schematic[i][j])){
             strZahl += schematic[i][j];
           } else {
             // Wurde schon eine Zahl gefunden?
             if(strZahl.length>0){
-              // Hier Nachbarn prüfen
-
+              // Sofort zählen wenn kein Punkt danach
+              if(schematic[i][j]!='.'){
+                gesamt1 += int.parse(strZahl);
+              } else {
+                // Sonst umfeld checken
+                if(istNachbarSybol(strZahl, schematic, i, j)){
+                  // i,j müsste feld danach sein wo keine Ziffer steht.
+                  gesamt1 += int.parse(strZahl);
+                }
+              }
               // Dann nächste Zahl finden
               strZahl ='';
             }
           }
       }
+      // Ist strZahl noch gefüllt?
+      if(strZahl.length>0){
+        print (strZahl);
+        if(istNachbarSybol(strZahl, schematic, i, j-1)){
+          // i,j müsste feld danach sein wo keine Ziffer steht.
+          gesamt1 += int.parse(strZahl);
+        }
+      }
     }
-    
-
-
-
-
-	  // print("Teil 1: "+gesamt1.toString());
+	  print("Teil 1: "+gesamt1.toString());
   } catch (e) {
     print('Fehler beim Lesen der Datei: $e');
   }
   
+}
+
+bool istNachbarSybol(String Zahl, List<List<String>> schematic, int i, int j)
+{
+  // **Zahl am Zeilenende?** // 
+  /* Block testen 1,4
+        links = j - länge(Zahl) - 1
+    i-1 .....
+    i   .111.
+    i+1 .....
+        rechts j 
+  */
+
+  int oben  = i - 1;
+  oben  = oben < 0 ? 0 : oben;
+
+  int unten = i + 1;
+  unten  = unten > (schematic.length-1) ? (schematic.length - 1) : unten;
+  
+  int links = j - Zahl.length -1;
+  links = links < 0 ? 0 : links;
+
+  int rechts = j;
+  
+
+  for(int ii=oben;ii<=unten;ii++){
+    for(int jj=links;jj<=rechts;jj++){
+      if(schematic[ii][jj]!='.' && !isDigit(schematic[ii][jj])){
+        // treffer
+        return true;
+      }
+    }  
+  }
+  return false;
 }
 
 // irgendwie in include umbauen
